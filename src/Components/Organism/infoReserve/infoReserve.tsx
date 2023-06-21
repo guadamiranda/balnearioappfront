a0c43330201992cda56d872bdf4cc38868fe64fe
Jonathan Calderon <jonathannahuelcalderon97@gmail.com>
2023-06-21 19:21:21 -0300

    commit 2

---
+++ src/Components/Organism/infoReserve/infoReserve.tsx
@@ -0,0 +1,15 @@
+import { HiOutlineIdentification } from "react-icons/hi";
+import style from "./infoReserve.module.scss";
+import Button from '../../Atoms/button/button';
+import { AiOutlineCar } from "react-icons/ai";
+import { useState } from "react";
+
+const InfoReserve = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default InfoReserve;

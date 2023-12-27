7a3ea71d8ecb558d3258cc0e8f9023e0fc581b01
Guadalupe Miranda <guadalupe.miranda@ymail.com>
2023-12-27 16:51:41 -0300

    Employee y Loader

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -2,23 +2,60 @@
 
 import HomeTemplate from '@/Components/templates/homeTemplate/homeTemplate';
 import HomeProfile from '@/Components/Molecules/HomeProfile/HomeProfile';
-import Button from '@/Components/Atoms/button/button';
+import Loader from '@/Components/Organism/loaderScreen/loader';
 import { useRouter, usePathname } from 'next/navigation';
+import sessionServices from '@/Services/sessionServices';
+import Button from '@/Components/Atoms/button/button';
+import AlertServices from '@/utils/AlertServices';
 import GuardLogin from '@/utils/guardLogin';
 import { useEffect, useState } from 'react';
-import AlertServices from '@/utils/AlertServices';
 import { isRouteAdmin } from './routes';
-import sessionServices from '@/Services/sessionServices';
 import style from './home.module.scss'
 
 const HomeComponent = () => {
     const router = useRouter();
-    const [isLoadingButtons, setIsLoadingButtons] = useState<boolean[]>([])
     const [isAdmin, setIsAdmin] = useState(false)
-    let lastId = 0;
+    const [loading, setLoading] = useState(false);
 
-    const handleClick = async (route:string, buttonId:number) => {
-        const loadingButtons = isLoadingButtons
+    const handleClick = (route: string) => {
+        setLoading(true);
+        router.push('/' + route);
+    }
+
+    useEffect(() => {
+        setIsAdmin(sessionServices.isAdmin())
+    }, [])
+
+
+    return (
+    <GuardLogin>
+            {!loading && (
+        <HomeTemplate title="Camping Los Nogales" subTitle="Administración">
+            <div className={style.homeContainer}>
+                <div className={style.homeContainer__leftSide}>
+                    <HomeProfile/>
+                </div>
+                <div className={style.homeContainer__rightSide}>
+                            <Button text='Reservas' type='primary' isFullWidth={true} onClickFunction={() => handleClick('Reserves')} />
+                            <Button text='Precios' type='primary' isFullWidth={true} onClickFunction={() => handleClick('ABMPrice')} />
+                            <Button text='Descuentos' type='primary' isFullWidth={true} onClickFunction={() => handleClick('ABMDiscounts')} />
+                            {/*<Button text='Roles' type={isAdmin? 'primary': 'disable'}  isFullWidth={true} onClickFunction={() => handleClick('ABMRol')}/>*/}
+                            <Button text='Empleados' type={'primary'} isFullWidth={true} onClickFunction={() => handleClick('ABMEmployee')} />
+                            <Button text='Reportes' type={'primary'} isFullWidth={true} onClickFunction={() => console.log('Soy Reportes')} />
+                </div>
+            </div>
+        </HomeTemplate>
+            )}
+            {loading && <Loader />}
+    </GuardLogin>
+    );
+};
+
+export default HomeComponent;
+
+
+/*
+ const loadingButtons = isLoadingButtons
         loadingButtons[lastId] = false;
         loadingButtons[buttonId] = true;
         lastId = buttonId;
@@ -39,31 +76,4 @@ const HomeComponent = () => {
             setIsLoadingButtons([false,false,false,false,false,false,false])
 
     }
-
-    useEffect(() => {
-        setIsAdmin(sessionServices.isAdmin())
-        setIsLoadingButtons([false,false,false,false,false,false,false])
-    }, [])
-
-    return (
-    <GuardLogin>
-        <HomeTemplate title="Camping Los Nogales" subTitle="Administración">
-            <div className={style.homeContainer}>
-                <div className={style.homeContainer__leftSide}>
-                    <HomeProfile/>
-                </div>
-                <div className={style.homeContainer__rightSide}>
-                    <Button text='Reservas' type='primary' isLoading={isLoadingButtons[0]} isFullWidth={true} onClickFunction={() => handleClick('Reserves',0) }/>
-                    <Button text='Precios' type='primary' isLoading={isLoadingButtons[1]} isFullWidth={true} onClickFunction={() => handleClick('ABMPrice',1)}/>
-                    <Button text='Descuentos' type='primary' isLoading={isLoadingButtons[2]} isFullWidth={true} onClickFunction={() => handleClick('ABMDiscounts',2)}/>
-                    {/*<Button text='Roles' type={isAdmin? 'primary': 'disable'} isLoading={isLoadingButtons[3]} isFullWidth={true} onClickFunction={() => handleClick('ABMRol',3)}/>*/ }
-                    <Button text='Empleados' type={'primary'} isLoading={isLoadingButtons[4]} isFullWidth={true} onClickFunction={() => handleClick('ABMEmployee',4)}/>
-                    <Button text='Reportes' type={'primary'} isLoading={isLoadingButtons[5]} isFullWidth={true} onClickFunction={() => console.log('Soy Reportes',5)}/>
-                </div>
-            </div>
-        </HomeTemplate>
-    </GuardLogin>
-    );
-};
-
-export default HomeComponent;
+    */
